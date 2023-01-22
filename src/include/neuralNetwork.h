9019cc8225d20cb640855c2b3940e4416d5a140b
HiddenRoom <bestwitherskele@gmail.com>
2023-01-22 00:30:00 -0700

    base

---
+++ src/include/neuralNetwork.h
@@ -0,0 +1,15 @@
+#ifndef NEURAL_NETWORK
+#define NEURAL_NETWORK
+
+#include <stdint.h>
+
+typedef struct NEURAL_NETWORK_STRUCT
+{
+  uint8_t layerNum;
+  uint8_t *layerSizes;
+
+  double *weights; /* could be matrix but will be parsed from 1D array to avoid cache annoyingness */
+  double *biases;
+} neuralNet_t;
+
+#endif
