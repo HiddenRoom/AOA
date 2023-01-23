671894d3b2189807fb84656fab47fbf08be416e2
HiddenRoom <bestwitherskele@gmail.com>
2023-01-23 00:36:48 -0700

    function prototypes for layout of network and matrices

---
+++ src/include/matrix.h
@@ -0,0 +1,14 @@
+#ifndef MATRIX
+#define MATRIX
+
+#include <stdint.h>
+
+typedef struct MATRIX_STRUCT
+{
+  uint8_t rowNum, colNum;
+  double **entries;
+} matrix_t;
+
+matrix_t *matrix_init(uint8_t rowNum, uint8_t colNum);
+
+#endif
--- src/include/neuralNetwork.h
+++ src/include/neuralNetwork.h
@@ -3,15 +3,17 @@
 
 #include <stdint.h>
 
+#include "matrix.h"
+
 typedef struct NEURAL_NETWORK_STRUCT
 {
   uint8_t layerNum;
   uint8_t *layerSizes;
 
-  double *weights; /* could be matrix but will be parsed from 1D array to avoid cache annoyingness */
-  double *biases;
+  matrix_t **weights; 
+  double **biases;
 } neuralNet_t;
 
-neuralNet_t *neuralNet_init(uint8_t layerNum, uint8_t *layerSizes) /* generate randomly seeded neural net */
+neuralNet_t *neuralNet_init(uint8_t layerNum, uint8_t *layerSizes); /* generate randomly seeded neural net */
 
 #endif
