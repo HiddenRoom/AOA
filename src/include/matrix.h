f0cdcc70a0ca04d13116b97864ae8e11b6dbf58c
Gamers Gaming <bestwitherskele@gmail.com>
2023-07-03 00:38:48 -0700

    add malloc error catcher header

--- src/include/matrix.h
+++ src/include/matrix.h
@@ -8,6 +8,8 @@ typedef struct MATRIX_STRUCT
   double **entries;
 } matrix_t;
 
+void nullCatchAndDie(void *ptr, char *msg);
+
 matrix_t *matrixInit(double coefficient, uint32_t rowNum, uint32_t colNum);
 
 void freeMatrix(matrix_t *matrix, uint32_t rowNum);
